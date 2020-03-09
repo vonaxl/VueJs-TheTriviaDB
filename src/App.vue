e31d3c138a204ec6b0920d7280725ca87e141674
vonaxl <vonaxlcorso@gmail.com>
2020-03-09 16:48:36 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <!-- <P>What category would you like to test?</P> -->
+
+    <div class="container">
+      <div class="row justify-content-center align-items-center" style="height:80vh">
+        <h3>Score! [{{score}}]</h3>
+        <div class="row">
+          <h2 class="col-md-12 text-center">{{ trivia.question }}</h2>
+          <div v-for="choice in choices" v-bind:key="choice" class="col-md-6">
+            <button @click="check(choice)" class="w-100" :disabled="clicked">{{choice}}</button>
+          </div>
+        </div>
+        <button @click="nextTest">Next Trivia</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import nav from "./components/Nav.vue";
+import Header from "./components/layout/Header";
+// import Trivias from "./components/Trivias";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Header
+    // Trivias
+  },
+  data() {
+    return {
+      trivia: [],
+      choices: [],
+      score: 0,
+      correctAnswer: "",
+      clicked: false
+    };
+  },
+  created() {
+    this.nextTest();
+  },
+  methods: {
+    reset() {
+      this.choices = [];
+      this.correctAnswer = "";
+      this.clicked = false;
+    },
+    nextTest() {
+      axios
+        .get("https://opentdb.com/api.php?amount=1")
+        .then(res => {
+          this.trivia = res.data.results[0];
+          // this.trivia.replace(/&quot;/g, '"');
+          this.choicesLoad(
+            this.trivia["correct_answer"],
+            this.trivia["incorrect_answers"]
+          );
+        })
+        .catch(err => console.log(err));
+    },
+    choicesLoad(choice1, choice2) {
+      this.reset();
+      this.correctAnswer = choice1;
+      this.choices.push(choice1);
+      choice2.forEach(choice => {
+        this.choices.push(choice);
+      });
+      this.shuffle(this.choices);
+      console.log(this.choices);
+    },
+    shuffle(a) {
+      var j, x, i;
+      for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+      }
+      return a;
+    },
+    check(answer) {
+      if (answer === this.correctAnswer) {
+        this.clicked = true;
+        this.score++;
+        alert("Goodjob!");
+      } else {
+        alert("WRONG!");
+      }
+      setTimeout(this.nextTest(), 1000);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="header bg-success text-center">
+    <h1>Trivia Axl</h1>
+  </header>
+</template>
+<script>
+export default {
+  name: "header"
+};
+</script>
